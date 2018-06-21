edbb61f633c5d8d9d9adc18ce9374cdb1d0b0014
juarisco <juarisco@outlook.com>
2018-06-21 17:11:26 -0500

    Factory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +13,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(User::class)->create(
+        [
+        	'email' => 'josef@mail.com',
+        	'name' => 'Josef'
+        ]);
+        factory(User::class)->create(
+        [
+        	'email' => 'bitfumes@mail.com',
+        	'name' => 'Bitfumes'
+        ]);
+        factory(User::class)->create(
+        [
+        	'email' => 'ankur@mail.com',
+        	'name' => 'Ankur'
+        ]);
     }
 }
